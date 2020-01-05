afe337b4776e78ae824f132972e2eddb77f5beb1
Lax <public@laxwashere.com>
2020-01-04 17:35:17 -0800

    ,

--- plugins/spacex.py
+++ plugins/spacex.py
@@ -14,4 +14,4 @@ def getTimeLeft(timestamp):
 def spacex(bot):
     data = urlr.urlopen(link).read().decode("utf-8")
     newdata = json.loads(data)
-    return "A {} is scheduled to launch {} from {} in {}".format(newdata["rocket"]["rocket_name"], newdata["mission_name"] newdata["launch_site"]["site_name"], getTimeLeft(newdata["launch_date_unix"]))
+    return "A {} is scheduled to launch {} from {} in {}".format(newdata["rocket"]["rocket_name"], newdata["mission_name"], newdata["launch_site"]["site_name"], getTimeLeft(newdata["launch_date_unix"]))
